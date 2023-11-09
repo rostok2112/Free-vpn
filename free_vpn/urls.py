dc39c49e34fb63aa884a609dd0a2e9e8582a2d6d
little-endian <rostok21122112@gmail.com>
2023-11-10 00:43:10 +0200

    Added favicon

--- free_vpn/urls.py
+++ free_vpn/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('',  include("app.urls")),
     path('admin/', admin.site.urls),
-    *static(settings.STATIC_URL, document_root=settings.BASE_DIR) # pathes for static files
+    *static(settings.STATIC_URL, document_root=settings.BASE_DIR), # pathes for static files
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
 ] 
