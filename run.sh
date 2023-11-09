cf2fc5f9b62ea394f57228ce748f2a1f49806e8f
little-endian <rostok21122112@gmail.com>
2023-11-09 23:30:04 +0200

    Fix build of docker container

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ set -x # enable print of debug logs
 cd /code/
 
 python manage.py migrate # Migrate DB
-python manage.py collectstatic # Collect static
+python manage.py collectstatic --noinput # Collect static
 python manage.py runserver 0.0.0.0:8005 # Run application
